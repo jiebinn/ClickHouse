d6cff50a7e44c0612437515e3da2a380ffa68b0d
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2026-07-02 13:23:36 +0000

    Revert second fix

--- src/Storages/MergeTree/IMergeTreeDataPart.cpp
+++ src/Storages/MergeTree/IMergeTreeDataPart.cpp
@@ -276,17 +276,10 @@ void IMergeTreeDataPart::MinMaxIndex::update(const Block & block, const NamesAnd
         FieldRef min_value;
         FieldRef max_value;
         const ColumnWithTypeAndName & column = block.getColumnOrSubcolumnByName(column_name);
-        ColumnPtr column_ptr = column.column;
-
-        /// `LowCardinality(Nullable)` must go through `getExtremesNullLast` as well: `ColumnLowCardinality::getExtremes`
-        /// ignores NULLs, and a range without NULL_LAST would let the index prune parts that contain NULL.
-        if (const auto * column_lc = typeid_cast<const ColumnLowCardinality *>(column_ptr.get()); column_lc && column_lc->nestedIsNullable())
-            column_ptr = column_lc->convertToFullColumn();
-
-        if (const auto * column_nullable = typeid_cast<const ColumnNullable *>(column_ptr.get()))
-            column_nullable->getExtremesNullLast(min_value, max_value, 0, column_ptr->size());
+        if (const auto * column_nullable = typeid_cast<const ColumnNullable *>(column.column.get()))
+            column_nullable->getExtremesNullLast(min_value, max_value, 0, column.column->size());
         else
-            column_ptr->getExtremes(min_value, max_value, 0, column_ptr->size());
+            column.column->getExtremes(min_value, max_value, 0, column.column->size());
 
         if (!initialized)
             hyperrectangle.emplace_back(min_value, true, max_value, true);
