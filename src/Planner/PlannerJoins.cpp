758a05fe7ae3bf6a92d9e2be9969fd18f4a11432
János Benjamin Antal <benjamin.antal@clickhouse.com>
2026-07-03 08:37:42 +0000

    Pass join_any_take_last_row through JoinSwitcher

--- src/Planner/PlannerJoins.cpp
+++ src/Planner/PlannerJoins.cpp
@@ -1313,7 +1313,8 @@ static std::shared_ptr<IJoin> tryCreateJoin(
         }
 
         if (MergeJoin::isSupported(table_join))
-            return std::make_shared<JoinSwitcher>(table_join, right_table_expression_header, stats_collecting_params);
+            return std::make_shared<JoinSwitcher>(
+                table_join, right_table_expression_header, params.join_any_take_last_row, stats_collecting_params);
         return std::make_shared<HashJoin>(
             table_join, right_table_expression_header, params.join_any_take_last_row, /*reserve_num_=*/0, /*instance_id_=*/"",
             /*use_two_level_maps_=*/false, stats_collecting_params);
