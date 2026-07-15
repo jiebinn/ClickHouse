880d15401bd353d49b9a65aabb1462daedf9c2b4
Matheus Agio Nerone <matheus.agio@clickhouse.com>
2026-07-15 10:11:58 +0000

    fixing unload test

--- src/Interpreters/ExternalLoader.cpp
+++ src/Interpreters/ExternalLoader.cpp
@@ -835,6 +835,7 @@ private:
         LOG_TRACE(log, "Unloading {} '{}'", type_name, name);
         info.object = nullptr;
         info.exception = nullptr;
+        info.loading_id = 0;
     }
 
     bool unload(Info * info)
